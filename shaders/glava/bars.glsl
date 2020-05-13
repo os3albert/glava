e84c53de0c22ef5ab4db3728b4ff37ef1896e1fa
Albert <47365826+os3albert@users.noreply.github.com>
2020-05-13 21:13:58 +0200

    Updated to black mode
    changed the amplify height and COLOR

--- shaders/glava/bars.glsl
+++ shaders/glava/bars.glsl
@@ -9,7 +9,7 @@
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 1
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 300
+#define AMPLIFY 150
 /* Whether the current settings use the alpha channel;
    enabling this is required for alpha to function
    correctly on X11 with `"native"` transparency */
@@ -17,7 +17,7 @@
 /* How quickly the gradient transitions, in pixels */
 #define GRADIENT 80
 /* Bar color. By default this provides a blue-white gradient. */
-#define COLOR @fg:mix(#3366b2, #a0a0b2, clamp(d / GRADIENT, 0, 1))
+#define COLOR @fg:mix(#000000, #FFFFFF, clamp(d / GRADIENT, 0, 1))
 /* Outline color. By default this provides a 'glint' outline based on the bar color */
 #define BAR_OUTLINE @bg:vec4(COLOR.rgb * 1.5, COLOR.a)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
